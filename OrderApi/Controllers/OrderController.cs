9c4178f61215bf4d1adb3e6ac786a0ccff7b0cb3
Razmik Anakhasyan <User@DESKTOP-DP52C93>
2023-10-03 16:37:15 +0400

    Add project files.

---
+++ OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IPublishEndpoint publishEndpoint;
+
+        public OrderController(IPublishEndpoint publishEndpoint)
+        {
+            this.publishEndpoint=publishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task Index(Order order) 
+        {
+            await publishEndpoint.Publish(order);
+        }
+    }
+}
